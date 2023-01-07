56a5f5e91bb684800d9ff77575274206b7b9b57e
Shivam95017 <120356210+Shivam95017@users.noreply.github.com>
2023-01-07 18:24:45 +0530

    Update Project weight conveter.py

--- Project weight conveter.py
+++ Project weight conveter.py
@@ -2,7 +2,7 @@ weight = int(input ('weight: '))
 unit= input('(L)bs or (K)g:')
 if unit.upper()=="L":
     converted= weight*0.45
-    print(f"You are {converted} Kilos ")
+    print(f"You are {converted} Lbs ")
 else:
     converted=weight//0.45
     print(f"you are {converted} Pounds ")
